737268c552dacc84603963277c382d1a3fbcea2d
dwheeler3039 <52417272+dwheeler3039@users.noreply.github.com>
2024-06-24 22:14:37 +0000

    more chpt 2 work

--- src/Ch02/02_09b/CodeDemo.cpp
+++ src/Ch02/02_09b/CodeDemo.cpp
@@ -3,8 +3,20 @@
 // Strings, by Eduardo Corpeño 
 
 #include <iostream>
+#include <string>
+#include <cstring>
 
 int main(){
+    const size_t LENGTH1 = 25;
+    char array_str1[LENGTH1] = "hello you people! ";
+    char array_str2[] = "what is going on?";
+
+    std::string std_str1 = "hi again. ";
+    std::string std_str2 = "how you doin?";
+
+    strncat(array_str1, array_str2, LENGTH1);
+    std::cout << array_str1 << std::endl;
+    std::cout << std_str1 + std_str2 << std::endl;
     
     std::cout << std::endl << std::endl;
     return (0);
