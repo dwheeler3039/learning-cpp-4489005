e36b3d891d53f10351dd1b3bb6faa8e4bb112f99
dwheeler3039 <52417272+dwheeler3039@users.noreply.github.com>
2024-06-26 22:16:10 +0000

    more chapter 3 work

--- src/Ch03/03_10b/CodeDemo.cpp
+++ src/Ch03/03_10b/CodeDemo.cpp
@@ -8,6 +8,17 @@
 #include "cow.h"
 
 int main(){
+    std::vector<cow> cattle;
+
+    cattle.push_back(cow("Besty", 6, cow_purpose::meat));
+    cattle.push_back(cow("Beaver", 4, cow_purpose::hide));
+    cattle.push_back(cow("Bork", 5, cow_purpose::pet));
+    cattle.push_back(cow("Bad", 2, cow_purpose::dairy));
+
+    std::cout << "The first cow is " << cattle.begin()->get_name() << std::endl;
+    std::cout << "Index 1 is " << cattle[1].get_name() << std::endl;
+    std::cout << "Next to last cow is " << prev(cattle.end(), 2)->get_name() << std::endl;
+    std::cout << "Last cow is " << (cattle.end() -1)->get_name() << std::endl;
     
     std::cout << std::endl << std::endl;
     return (0);
